f264ec01e8d9ae6c87b70c69928efc5666ab7724
Alicejp <alail5077@gmail.com>
2021-09-23 19:23:16 +0100

    Add scene manager

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other) {
         switch (other.gameObject.tag)
         {
-            case "Finish":
-                Debug.Log("Finish");
+            case "Start":
+                Debug.Log("Start");
                 break;
-            
+            case "Finish":
+                LoadNextScene();
+                break;
             default:
-                Debug.Log("default");
+                ReloadScene();
                 break;
         }
+    }
+
+    private void LoadNextScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        if (index == SceneManager.sceneCountInBuildSettings-1)
+        {
+            Debug.Log("Last scene, you won !! Congrats");
+        }
+        else
+        {
+            SceneManager.LoadScene(index+1);
+            Debug.Log("Load Next Scene");
+        }
+        
+    }
+
+    private void ReloadScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index);
+        Debug.Log("Restart");
     }
 }
