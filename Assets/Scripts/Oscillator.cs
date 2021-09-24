eb7bde3d5b0601b23ce6f2a80c2050d5be6d009a
Alicejp <alail5077@gmail.com>
2021-09-24 20:13:58 +0100

    add oscillator

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    
+    public Vector3 movementVector;
+    [Range(-1, 1)] public float movementFactor;
+    public float periods = 2f;
+
+    const float TAU = Mathf.PI * 2;
+    private Vector3 offest;
+    private Vector3 staringPosition;
+    private float cycles;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        staringPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (periods == Mathf.Epsilon)
+            return;
+        
+        cycles = Time.time / periods;
+
+        movementFactor = Mathf.Sin(TAU * cycles);
+        offest = movementFactor * movementVector * Time.deltaTime;
+
+        transform.position += offest;
+    }
+}
